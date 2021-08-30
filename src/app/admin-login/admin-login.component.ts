6c7d97c44c9e72b2ad4d99c8d15bbc858254113f
sakshi2009 <sakshiandani20@gmail.com>
2021-08-30 23:40:45 +0530

    2 files modified, 3 added

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-login',
@@ -7,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
   submit(){
-    alert("Logged in");
+    this.router.navigate(['/adminmenu'])
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { GuestMenuComponent } from './guest-menu/guest-menu.component';
+import { AdminMenuComponent } from './admin-menu/admin-menu.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,
@@ -16,9 +17,10 @@ import { GuestMenuComponent } from './guest-menu/guest-menu.component';
     {path:'',component:LoginComponent},
     {path:'guestlogin',component:GuestLoginComponent},
     {path:'adminlogin',component:AdminLoginComponent},
-    {path:'guestmenu', component:GuestMenuComponent}
+    {path:'guestmenu', component:GuestMenuComponent},
+    {path:'adminmenu', component:AdminMenuComponent},
   ]) ],
-  declarations: [ AppComponent, HelloComponent,GuestLoginComponent,AdminLoginComponent ,LoginComponent, GuestMenuComponent],
+  declarations: [ AppComponent, HelloComponent,GuestLoginComponent,AdminLoginComponent ,LoginComponent, GuestMenuComponent, AdminMenuComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
